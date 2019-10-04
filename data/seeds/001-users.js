b1911d683351eac2f84cd63fb48847e1c5d71a9b
JacobLayton <jlayjlay3@gmail.com>
2019-10-03 19:01:23 -0700

    Seed file for users complete with faker entries

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,30 +1,25 @@
+const faker = require("faker");
+
+const createFakeUser = () => ({
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  email: faker.internet.email(),
+  firebaseId: faker.random.uuid(),
+  picture: faker.image.food(),
+  lat: faker.address.latitude(),
+  lon: faker.address.longitude()
+});
+
 exports.seed = function(knex) {
+  let fakeUsers = [];
+  for (let i = 1; i <= 90; i++) {
+    fakeUsers.push(createFakeUser());
+  }
   // Deletes ALL existing entries
   return knex("users")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("users").insert([
-        {
-          id: 1,
-          firstName: "Bobby",
-          lastName: "Brown",
-          email: "bobby@brown.com",
-          firebaseId: "FIREBASEID",
-          picture: "picture.jpg",
-          lat: "-122.4587",
-          lon: "79.5634"
-        },
-        {
-          id: 2,
-          firstName: "Whitney",
-          lastName: "Houston",
-          email: "Whitney@Houston.com",
-          firebaseId: "FIREBASEID2",
-          picture: "picture.jpg",
-          lat: "-122.4587",
-          lon: "79.5634"
-        }
-      ]);
+      return knex("users").insert(fakeUsers);
     });
 };
