374f8054025ae0d29d1df3ec04d07b97d3a391f7
ltrii <lelandtr@gmail.com>
2019-10-08 14:43:18 -0700

    Migration updates

---
+++ data/migrations/20191008142810_orders.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("orders", u => {
+        u.increments('id')
+        u.integer("userId")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+        u.integer("farmerId")
+            .unsigned()
+            .notNullable()
+            .references("farmerId")
+            .inTable("farmers")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+        u.date("date");
+      });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("orders");
+};
